bb16a34915629f6e00ce1d7473ac4d45a961a801
Damien Cuda <contact@damiencuda.com>
2022-10-31 17:37:05 +0100

    Commentaires codes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    const modal = document.querySelector('#login_modal'); //Initialisation de variable en vanillaJs pour utiliser la méthode showModal()
-    modal.showModal();
+    const modal = document.querySelector('#login_modal'); //Initialisation de variable en vanillaJS pour utiliser la méthode showModal()
+    modal.showModal(); //La modal apparait dès le lenacement du jeu
 
     const pseudo_input = $('#pseudo_input');
     const dificulty_level = $('#level_choice_wrapper input');
@@ -11,18 +11,19 @@ document.addEventListener("DOMContentLoaded", function(){
 
     $(start_btn).click(start_game)
 
+    //Fonction de vérifiacation du pseudo et verification du niveau de difficulté
     function start_game(){
-        var player_pseudo = $(pseudo_input).val();
+        var player_pseudo = $(pseudo_input).val(); //récupération pseudo
         if (player_pseudo == ""){
-            $(error_message).html('Vous devez entrer votre pseudo pour jouer ;)')
+            $(error_message).html('Vous devez entrer votre pseudo pour jouer ;)') //Affichage message d'erreur si pas de pseudo choisit
             
         }else{
             $.each( dificulty_level, function( key, value ) {
                 if (value.checked){
-                    difficulty =  value.value;
+                    difficulty =  value.value; //récupération niveau de dificulté choisit
                 }
               });
-            modal.style.display = "none";
+            modal.style.display = "none"; //La modale est caché
         }
     }
 });
