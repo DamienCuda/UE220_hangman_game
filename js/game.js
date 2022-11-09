43fc46f928b64a8f119c82f0c46883a732b28d23
Damien Cuda <contact@damiencuda.com>
2022-11-09 11:40:58 +0100

    Correction bug clavier

--- js/game.js
+++ js/game.js
@@ -16,10 +16,19 @@ $(document).ready(function(){
     generate_keyboard();                                //Le clavier est généré
     //choix aléatoire du mot à injecter dans la variable mystery_word
     hidden_word(mystery_word);                          //Les tirets représentant le mot mystère sont générés en fonction de la longueur
-    user_keyboard_listener();                           //Le clavier physique est écouté
-    virtuel_keyboard_listener();                        //Le clavier virtuel est écouté
+ 
     
-
+    /***********ÉVENEMENTS*********/
+    
+    //Écoute du clavier virtuel
+    var letters = $('.letter');
+    $(letters).each(function(key, value){
+        $(value).click(function(){
+            choosen_letter = value.innerHTML;
+            console.log(choosen_letter);
+        })
+    });
+    
     /***********LES FONCTIONS*********/
 
     //Fonction de génération du mot mystère caché
@@ -28,7 +37,6 @@ $(document).ready(function(){
             $("#mystery_word_container").append(`<span class="mystery_letters frederica_font">_</span>`)
         }
     };
-
     //Fonction de génération du clavier virtuel
     function generate_keyboard(){
         for( var i = 65; i <= 90; i++){
@@ -36,23 +44,14 @@ $(document).ready(function(){
            $("#keyboard_container").append(`<span class="letter frederica_font">${letter}</span>`);
         };
     };
+});
 
-    //Fonction d'écoute du clavier virtuel
-    function virtuel_keyboard_listener(){
-        var letters = $('.letter');
-        $.each(letters.click((letter)=> {
-            choosen_letter= letter.currentTarget.innerHTML;
-            //Ajouter la fonction de vérification ici
-        }));
-    };
+/*********Écoute du clavier physique tout le document************/
 
-    //Fonction d'écoute du clavier de l'utilisateur
-    function user_keyboard_listener(){
-        $(document).keydown(function(event){
-            if ((event.keyCode >= 65) && (event.keyCode <= 90)){ //Seul les lettres avec le keycode entre 65 et 90 sont ajoutées à la variable choosen_letter
-                choosen_letter = event.key.toUpperCase();
-                //Ajouter la fonction de vérification ici
-            }
-        });
-    };
+$(document).keydown(function(event){
+    if ((event.keyCode >= 65) && (event.keyCode <= 90)){ //Seul les lettres avec le keycode entre 65 et 90 sont ajoutées à la variable choosen_letter
+        choosen_letter = event.key.toUpperCase();
+        console.log(choosen_letter);
+        //Ajouter la fonction de vérification ici
+    }
 });
