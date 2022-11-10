2849d5e145befc7b4bb7e20f2b42cf7bba231036
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2022-11-10 18:22:07 +0100

    Fonction verif

---
+++ js/games.js
@@ -0,0 +1,163 @@
+import * as hangman from "./hangman.js";
+
+$(document).ready(function(){
+
+    /***********LES VARIABLES*********/
+
+    var mystery_word = "test"; // Mot à découvrir
+    var choosen_letter = ""; // Lettre choisit par le joueur au click ou au clavier
+    var player_pseudo = sessionStorage.getItem('pseudo'); //Récupération du pseudo de session
+    var dificulty_level = sessionStorage.getItem('level'); //Récupération du niveau de session
+    var error_counter = 0;
+    
+
+    /***********INITIALISATION DU JEU*********/
+
+    $("#current_difficulty").html(dificulty_level);     //Injection du niveau de difficulté
+    $("#player_pseudo").html(player_pseudo);            //Injection du pseudo
+
+    generate_keyboard();                                //Le clavier est généré
+                                                        //choix aléatoire du mot à injecter dans la variable mystery_word
+    hidden_word(mystery_word);                          //Les tirets représentant le mot mystère sont générés en fonction de la longueur
+
+    hangman_steps(7)
+
+    
+    /***********JEU EN COURS (peut-être joué au clavier physique et virtuel)*********/
+    
+    //Écoute du clavier phisique
+    $(document).keydown(function(event){
+        if ((event.keyCode >= 65) && (event.keyCode <= 90)){ //Seul les lettres avec le keycode entre 65 et 90 sont ajoutées à la variable choosen_letter
+            choosen_letter = event.key.toUpperCase();
+            show_letters(0, choosen_letter);
+        }
+    });
+     
+    //Écoute du clavier virtuel
+    var letters = $('.letter');
+    let mysteryWordArray = [];
+    let wordTemp = [];
+
+    mysteryWordArray = mystery_word.toUpperCase().split('')
+
+    for(let i = 0; i < mysteryWordArray.length; i++){
+        wordTemp.push("_");
+    }
+
+    $(letters).each(function(key, value){
+        $(value).click(function(){
+            verif(choosen_letter,value.textContent);
+        })
+    });
+
+    function verif(choosen_letter, value){
+
+        let error = false;
+        for(let i = 0; i < mysteryWordArray.length; i++){
+            if(mysteryWordArray.includes(value)){
+                if(mysteryWordArray[i].indexOf(value) !== -1){
+                    wordTemp[i] = value
+                    show_letters(i, value);
+                }
+            }else{
+                error = true;
+            }
+        }
+
+        if(error == true){
+            /* ICI on gere les erreurs */
+            alert("erreur")
+        }
+
+        console.log(mysteryWordArray)
+        console.log(wordTemp)
+
+        let tempWord = $(".mystery_letters")
+        let tempWordSize = 0;
+        $(tempWord).each((key, value) =>{
+            if(value.textContent !== "_"){
+                tempWordSize++
+            }
+        })
+
+        /* Je ne sais pas à quoi servais exactement cette ligne damien donc je l'ai commenté au cas ou c'était important */
+        //choosen_letter = value.innerHTML;
+    }
+
+    
+    /***********LES FONCTIONS*********/
+
+    //Fonction de génération du mot mystère caché
+    function hidden_word(word){
+        for (var i = 0 ; i < word.length; i++){
+            $("#mystery_word_container").append(`<span class="mystery_letters frederica_font">_</span>`)
+        }
+    };
+    //Fonction de génération du clavier virtuel
+    function generate_keyboard(){
+        for( var i = 65; i <= 90; i++){
+           var letter = String.fromCharCode(i);
+           $("#keyboard_container").append(`<span class="letter frederica_font">${letter}</span>`);
+        };
+    };
+
+    //Fonction d'affichage de la lettre si bonne pioche
+    function show_letters(index, letter){ //Prend 2 paramètres l'index de la lettre dans le mot et la lettre
+        var mystery_letters = $('.mystery_letters');
+        $(mystery_letters).each((key, value) =>{
+            if(key === index){
+                value.innerHTML = letter;
+            }
+        })
+    };
+
+    //fonction d'affichage du hangman en fonction du nombre d'erruer du joueur
+    function hangman_steps(error_counter){
+        switch(error_counter){
+            case 1:
+                hangman.gibbet();
+                break;
+            case 2:
+                hangman.headHangman();
+                break;
+            case 3:
+                hangman.bodyHangman();
+                break;
+            case 4:
+                hangman.armRightHangman();
+                break;
+            case 5:
+                hangman.armLeftHangman();
+                break;
+            case 6:
+                hangman.legRightHangman();
+                break;
+            case 7:
+                hangman.legLeftHangman();
+                break;
+        };
+    };
+
+});
+
+
+/*
+while(error_counter < 7){
+
+};
+    
+if error_counter == 7 Fonction GAME OVER
+
+FONCTION SCORE
+
+FONCTION RESTART
+
+FONCTION verif(choosen_letter)
+    si lettre dans le mot 
+        fonction affichage
+            si tableau_temp == mystery_word
+            fonction WIN
+            break
+    sinon error_counter ++
+    fonction afffichage canvas
+*/
--- js/hangman.js
+++ js/hangman.js
@@ -1,126 +1,112 @@
-window.addEventListener("DOMContentLoaded", function(){
-    var hangman = document.getElementById("hangman");
-    var white = "rgb(255,255,255)";
+var hangman = document.getElementById("hangman");
+var white = "rgb(255,255,255)";
 
-    init();
+export function headHangman(){
+    gibbet();
+    let headHangman = hangman.getContext("2d");
+    headHangman.fillStyle = white;
+    headHangman.strokeStyle = white;
+    headHangman.lineWidth = 3;
+    headHangman.beginPath();
+    headHangman.arc(150, 100, 20, 0, 2 * Math.PI, false);
+    headHangman.stroke();
+    headHangman.closePath();
+}
 
-    function init(){
-        /*
-            Pour afficher un partie du pendu, il suffit d'appeler la fonction de la partie souhaiter.
-            Note: Gibbet signifie potence en anglais.
-         */
+export function bodyHangman(){
+    headHangman();
+    let bodyHangman = hangman.getContext("2d");
+    bodyHangman.fillStyle = white;
+    bodyHangman.strokeStyle = white;
+    bodyHangman.beginPath();
+    bodyHangman.lineWidth = 3;
+    bodyHangman.moveTo(150,120);
+    bodyHangman.lineTo(150,200);
+    bodyHangman.stroke();
+}
 
-        gibbet();
-        headHangman();
-        bodyHangman();
-        armRightHangman();
-        armLeftHangman();
-        legRightHangman();
-        legLeftHangman();
+export function armRightHangman(){
+    bodyHangman();
+    let armRightHangman = hangman.getContext("2d");
+    armRightHangman.fillStyle = white;
+    armRightHangman.strokeStyle = white;
+    armRightHangman.lineWidth = 3;
+    armRightHangman.beginPath();
+    armRightHangman.moveTo(180,160);
+    armRightHangman.lineTo(150,130);
+    armRightHangman.stroke();
+}
 
-    }
+export function armLeftHangman(){
+    armRightHangman();
+    let armLeftHangman = hangman.getContext("2d");
+    armLeftHangman.fillStyle = white;
+    armLeftHangman.strokeStyle = white;
+    armLeftHangman.lineWidth = 3;
+    armLeftHangman.beginPath();
+    armLeftHangman.moveTo(120,160);
+    armLeftHangman.lineTo(150,130);
+    armLeftHangman.stroke();
+}
 
-    function headHangman(){
-        headHangman = hangman.getContext("2d");
-        headHangman.fillStyle = white;
-        headHangman.strokeStyle = white;
-        headHangman.lineWidth = 3;
-        headHangman.beginPath();
-        headHangman.arc(150, 100, 20, 0, 2 * Math.PI, false);
-        headHangman.stroke();
-        headHangman.closePath();
-    }
+export function legRightHangman(){
+    armLeftHangman();
+    let legRightHangman = hangman.getContext("2d");
+    legRightHangman.fillStyle = white;
+    legRightHangman.strokeStyle = white;
+    legRightHangman.lineWidth = 3;
+    legRightHangman.beginPath();
+    legRightHangman.moveTo(180,230);
+    legRightHangman.lineTo(150,200);
+    legRightHangman.stroke();
+}
 
-    function bodyHangman(){
-        bodyHangman = hangman.getContext("2d");
-        bodyHangman.fillStyle = white;
-        bodyHangman.strokeStyle = white;
-        bodyHangman.beginPath();
-        bodyHangman.lineWidth = 3;
-        bodyHangman.moveTo(150,120);
-        bodyHangman.lineTo(150,200);
-        bodyHangman.stroke();
-    }
+export function legLeftHangman(){
+    legRightHangman();
+    let legLeftHangman = hangman.getContext("2d");
+    legLeftHangman.fillStyle = white;
+    legLeftHangman.strokeStyle = white;
+    legLeftHangman.lineWidth = 3;
+    legLeftHangman.beginPath();
+    legLeftHangman.moveTo(120,230);
+    legLeftHangman.lineTo(150,200);
+    legLeftHangman.stroke();
+}
 
-    function armRightHangman(){
-        armRightHangman = hangman.getContext("2d");
-        armRightHangman.fillStyle = white;
-        armRightHangman.strokeStyle = white;
-        armRightHangman.lineWidth = 3;
-        armRightHangman.beginPath();
-        armRightHangman.moveTo(180,160);
-        armRightHangman.lineTo(150,130);
-        armRightHangman.stroke();
-    }
+export function gibbet(){
+    let gibbetHead = hangman.getContext("2d");
+    gibbetHead.fillStyle = white;
+    gibbetHead.strokeStyle = white;
+    gibbetHead.lineWidth = 3;
+    gibbetHead.beginPath();
+    gibbetHead.moveTo(150,60);
+    gibbetHead.lineTo(150,80);
+    gibbetHead.stroke();
 
-    function armLeftHangman(){
-        armLeftHangman = hangman.getContext("2d");
-        armLeftHangman.fillStyle = white;
-        armLeftHangman.strokeStyle = white;
-        armLeftHangman.lineWidth = 3;
-        armLeftHangman.beginPath();
-        armLeftHangman.moveTo(120,160);
-        armLeftHangman.lineTo(150,130);
-        armLeftHangman.stroke();
-    }
+    let gibbetTraverse = hangman.getContext("2d");
+    gibbetTraverse.fillStyle = white;
+    gibbetTraverse.strokeStyle = white;
+    gibbetTraverse.lineWidth = 3;
+    gibbetTraverse.beginPath();
+    gibbetTraverse.moveTo(64,60);
+    gibbetTraverse.lineTo(152,60);
+    gibbetTraverse.stroke();
 
-    function legRightHangman(){
-        legRightHangman = hangman.getContext("2d");
-        legRightHangman.fillStyle = white;
-        legRightHangman.strokeStyle = white;
-        legRightHangman.lineWidth = 3;
-        legRightHangman.beginPath();
-        legRightHangman.moveTo(180,230);
-        legRightHangman.lineTo(150,200);
-        legRightHangman.stroke();
-    }
+    let gibbetVertical = hangman.getContext("2d");
+    gibbetVertical.fillStyle = white;
+    gibbetVertical.strokeStyle = white;
+    gibbetVertical.lineWidth = 3;
+    gibbetVertical.beginPath();
+    gibbetVertical.moveTo(65,60);
+    gibbetVertical.lineTo(65,390);
+    gibbetVertical.stroke();
 
-    function legLeftHangman(){
-        legLeftHangman = hangman.getContext("2d");
-        legLeftHangman.fillStyle = white;
-        legLeftHangman.strokeStyle = white;
-        legLeftHangman.lineWidth = 3;
-        legLeftHangman.beginPath();
-        legLeftHangman.moveTo(120,230);
-        legLeftHangman.lineTo(150,200);
-        legLeftHangman.stroke();
-    }
-
-    function gibbet(){
-        gibbetHead = hangman.getContext("2d");
-        gibbetHead.fillStyle = white;
-        gibbetHead.strokeStyle = white;
-        gibbetHead.lineWidth = 3;
-        gibbetHead.beginPath();
-        gibbetHead.moveTo(150,60);
-        gibbetHead.lineTo(150,80);
-        gibbetHead.stroke();
-
-        gibbetTraverse = hangman.getContext("2d");
-        gibbetTraverse.fillStyle = white;
-        gibbetTraverse.strokeStyle = white;
-        gibbetTraverse.lineWidth = 3;
-        gibbetTraverse.beginPath();
-        gibbetTraverse.moveTo(58,60);
-        gibbetTraverse.lineTo(152,60);
-        gibbetTraverse.stroke();
-
-        gibbetVertical = hangman.getContext("2d");
-        gibbetVertical.fillStyle = white;
-        gibbetVertical.strokeStyle = white;
-        gibbetVertical.lineWidth = 3;
-        gibbetVertical.beginPath();
-        gibbetVertical.moveTo(60,60);
-        gibbetVertical.lineTo(60,397);
-        gibbetVertical.stroke();
-
-        gibbetSocle = hangman.getContext("2d");
-        gibbetSocle.fillStyle = white;
-        gibbetSocle.strokeStyle = white;
-        gibbetSocle.lineWidth = 3;
-        gibbetSocle.beginPath();
-        gibbetSocle.moveTo(120,398);
-        gibbetSocle.lineTo(0,398);
-        gibbetSocle.stroke();
-    }
-})
+    let gibbetSocle = hangman.getContext("2d");
+    gibbetSocle.fillStyle = white;
+    gibbetSocle.strokeStyle = white;
+    gibbetSocle.lineWidth = 3;
+    gibbetSocle.beginPath();
+    gibbetSocle.moveTo(125,390);
+    gibbetSocle.lineTo(5,390);
+    gibbetSocle.stroke();
+}
