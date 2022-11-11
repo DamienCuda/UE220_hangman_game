fac2ce07121503f8426f313466e36d9bf100c642
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2022-11-11 17:32:00 +0100

    Update function loose

--- js/games.js
+++ js/games.js
@@ -136,11 +136,36 @@ $(document).ready(function(){
                 break;
             case 7:
                 hangman.legLeftHangman();
+                loose();
                 break;
         };
     };
 
 });
+
+function loose(){
+    let modalLoose =
+        '<div className="modal" tabIndex="-1" role="dialog" style="display:block">' +
+            '<div className="modal-dialog" role="document">' +
+                '<div className="modal-content">' +
+                    '<div className="modal-header">' +
+                        '<h5 className="modal-title">Modal title</h5>' +
+                        '<button type="button" className="close" data-dismiss="modal" aria-label="Close">' +
+                            '<span aria-hidden="true"></span>' +
+                        '</button>' +
+                    '</div>' +
+                    '<div className="modal-body">' +
+                        '<p>Modal body text goes here.</p>' +
+                    '</div>' +
+                    '<div className="modal-footer">' +
+                        '<button type="button" className="btn btn-primary">Save changes</button>' +
+                        '<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>';
+    document.getElementById("game_container").innerHTML += modalLoose
+}
 
 
 /*
